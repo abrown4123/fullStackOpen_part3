ea825b38cc2da694cda24d7d7348954acb5abe36
Anthony Brown <abrown4123@gmail.com>
2021-11-21 00:10:38 -0600

    Add front end build to project to deploy to Heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express"),
       cors = require("cors");
       app = express();
 
-// app.use(express.static('build'))
+app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
