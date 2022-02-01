97552c9ed7b064a1ad88b2e669d2c499719e47fc
Anthony Brown <abrown4123@gmail.com>
2022-01-31 22:19:02 -0600

    Exercises 3.15 & 3.16

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express"),
       morgan = require("morgan"),
       Entry = require("./models/entry"),
+      { unknownEndpoint, errorHandler } = require("./middleware/error")
       cors = require("cors"),
       app = express();
-
+// console.log(errorHandlers)
 app.use(express.static('build'))
+
 app.use(express.json())
 app.use(cors())
 
@@ -91,11 +93,16 @@ app.post("/api/persons", (request, response) => {
   })
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  people = people.filter(entry => entry.id !== id)
-  response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+  Entry.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
